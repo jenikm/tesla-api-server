26c1ba2fc482cccfaf873885a711d61d22cdcdca
haha150 <alisymeri@gmail.com>
2022-03-28 23:59:57 +0200

    Update gen.rb

--- gen.rb
+++ gen.rb
@@ -16,3 +16,12 @@ print("\n")
 test = "https://auth.tesla.com/oauth2/v3/authorize?redirect_uri=https://auth.tesla.com/void/callback&response_type=code&client_id=ownerapi&scope=offline_access+openid+email&code_challenge="+code_challenge+"&code_challenge_method=S256"
 print(test)
 print("\n")
+=begin
+curl -X POST -k -H 'Content-Type: application/json' -i 'https://auth.tesla.com/oauth2/v3/token' --data '{
+  "grant_type": "authorization_code",
+  "client_id": "ownerapi",
+  "code": "",
+  "code_verifier": "",
+  "redirect_uri": "https://auth.tesla.com/void/callback"
+}'
+=end
