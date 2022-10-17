ed50fe4bcab95baa6521ba1636f703b0710cb3bf
Yevgeniy Magdel <yevgeniy.magdel@rallyhealth.com>
2022-10-16 21:24:44 -0500

    Making script more automated

--- gen.rb
+++ gen.rb
@@ -1,8 +1,9 @@
 require "date"
 require "base64"
+require 'net/http'
+require 'digest'
+require 'json'
 
-require "faraday"
-require "faraday_middleware"
 
 code_verifier = rand(36**86).to_s(36)
 code_challenge = Base64.urlsafe_encode64(Digest::SHA256.hexdigest(code_verifier))
@@ -18,12 +19,20 @@ print("Visit the following link:")
 print("\n")
 print(url)
 print("\n")
-=begin
-curl -X POST -k -H 'Content-Type: application/json' -i 'https://auth.tesla.com/oauth2/v3/token' --data '{
+system('open', url)
+
+
+
+print "Enter 'code' from URL redirect: "
+code=gets.chomp
+uri = URI('https://auth.tesla.com/oauth2/v3/token')
+result = Net::HTTP.post(uri, {
   "grant_type": "authorization_code",
   "client_id": "ownerapi",
-  "code": "",
-  "code_verifier": "",
+  "code": code,
+  "code_verifier": code_verifier,
   "redirect_uri": "https://auth.tesla.com/void/callback"
-}'
-=end
+}.to_json, "Content-Type" => "application/json")
+
+puts JSON.pretty_generate(JSON.parse(result.body))
+
